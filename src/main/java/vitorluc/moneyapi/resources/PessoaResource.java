c73d8c7e508acb20c449ec0e7e093d06fd2864ae
VitorLucas98 <vitorluc.2013@gmail.com>
2021-08-28 17:51:38 -0300

    Buscar por id de Pessoas, ok !

--- src/main/java/vitorluc/moneyapi/resources/PessoaResource.java
+++ src/main/java/vitorluc/moneyapi/resources/PessoaResource.java
@@ -3,6 +3,7 @@ package vitorluc.moneyapi.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vitorluc.moneyapi.services.PessoaService;
@@ -21,4 +22,10 @@ public class PessoaResource {
     public ResponseEntity<List<PessoaDTO>> findAll(){
         return ResponseEntity.ok(service.findAll());
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<PessoaDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
 }
--- src/main/java/vitorluc/moneyapi/services/PessoaService.java
+++ src/main/java/vitorluc/moneyapi/services/PessoaService.java
@@ -3,11 +3,15 @@ package vitorluc.moneyapi.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import vitorluc.moneyapi.entities.Pessoa;
 import vitorluc.moneyapi.repositories.PessoaRepository;
 import vitorluc.moneyapi.services.dtos.PessoaDTO;
+import vitorluc.moneyapi.services.exceptions.ObjectNotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,5 +22,11 @@ public class PessoaService {
     @Transactional(readOnly = true)
     public List<PessoaDTO> findAll(){
         return repository.findAll().stream().map(x -> new PessoaDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public PessoaDTO findById(Long id){
+        Pessoa pes = repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Pessoa de id: "+ id +", não encontrada !"));
+        return new PessoaDTO(pes);
     }
 }
